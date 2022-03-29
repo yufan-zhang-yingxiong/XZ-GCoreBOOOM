5ab008e007214a68155d30f92efbaa73b157eaae
Yarpen Wong <b6462625302185@outlook.com>
2022-03-29 14:39:28 +0800

    Add ambient audio
    + Add ambient audio
    + Add bloom effect
    * Changed prefab appearance

---
+++ Assets/Scripts/AmbientController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmbientController : MonoBehaviour
+{
+    public AudioSource source;
+    public AudioClip clip_Amb_1;
+    public AudioClip clip_Amb_2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //source.clip = clip_Amb_1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
